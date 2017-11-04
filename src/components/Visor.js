24425aaac65bbf0f5185bb3519dc4bd2f945c302
Ricardo Cardoso <rick.duk@gmail.com>
2017-11-04 16:08:29 -0200

    first commit

---
+++ src/components/Visor.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, TextInput, StyleSheet } from 'react-native'
+
+export default props => (
+      <View>
+      <TextInput style={styles.visor}
+        placeholder='Resultado'
+        value={props.resultado}
+      />
+    </View>
+)
+const styles = StyleSheet.create ({
+  visor : {
+    height: 100,
+    fontSize: 30
+  }
+})
